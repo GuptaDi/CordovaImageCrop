6fa91e3af7662463183b9039a6804aaa6e78b258
Piyush Mittal <m@piyushmittal.com>
2016-12-10 11:59:22 +0530

    Plugin update

--- src/android/ImageCropPlugin.java
+++ src/android/ImageCropPlugin.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 
-import com.soundcloud.android.crop.Crop;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -16,6 +15,7 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -23,15 +23,15 @@ public class ImageCropPlugin extends CordovaPlugin {
     private CallbackContext callbackContext;
     private Uri inputUri;
     private Uri outputUri;
+    public static final int RESULT_OK  = -1;
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-      if (action.equals("cropImage")) {
+      if (action.equals("ImageCropPlugin")) {
           String imagePath = args.getString(0);
 
           this.inputUri = Uri.parse(imagePath);
-          this.outputUri = Uri.fromFile(new File(getTempDirectoryPath() + "/" + System.currentTimeMillis()+ "-cropped.jpg"));
-
+          
           PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
           pr.setKeepCallback(true);
           callbackContext.sendPluginResult(pr);
@@ -40,77 +40,38 @@ public class ImageCropPlugin extends CordovaPlugin {
           cordova.setActivityResultCallback(this);
 
            
-        CropImage.activity(imageUri)
+        CropImage.activity(inputUri)
                 .setGuidelines(CropImageView.Guidelines.ON)
                 .setMultiTouchEnabled(true)
-                .start(this);
-    }
-          // Crop.of(this.inputUri, this.outputUri)
-          //         .asSquare()
-          //         .start(cordova.getActivity());
-
+                .start(cordova.getActivity());
+       
           return true;
       }
       return false;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         // handle result of CropImageActivity
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-                ((ImageView) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
-                Toast.makeText(this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
+               // ((ImageView) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
+              JSONObject uriObj = new JSONObject();
+                try {
+                    uriObj.put("URI", result.getUri().toString());
+                    this.callbackContext.success(uriObj);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+              
+
+             //   Toast.makeText(this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
+             //   Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
             }
         }
-    }
-
-    private String getTempDirectoryPath() {
-        File cache = null;
-
-        // SD Card Mounted
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            cache = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
-                    "/Android/data/" + cordova.getActivity().getPackageName() + "/cache/");
-        }
-        // Use internal storage
-        else {
-            cache = cordova.getActivity().getCacheDir();
-        }
-
-        // Create the cache directory if it doesn't exist
-        cache.mkdirs();
-        return cache.getAbsolutePath();
-    }
-
-    public Bundle onSaveInstanceState() {
-        Bundle state = new Bundle();
-
-        if (this.inputUri != null) {
-            state.putString("inputUri", this.inputUri.toString());
-        }
-
-        if (this.outputUri != null) {
-            state.putString("outputUri", this.outputUri.toString());
-        }
-
-        return state;
-    }
-
-    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
-
-        if (state.containsKey("inputUri")) {
-            this.inputUri = Uri.parse(state.getString("inputUri"));
-        }
-
-        if (state.containsKey("outputUri")) {
-            this.inputUri = Uri.parse(state.getString("outputUri"));
-        }
-
-        this.callbackContext = callbackContext;
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
